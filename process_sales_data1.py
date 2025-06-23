92d76e0aa766e28233271e5ae10069ffb802b763
Balaji M <73985468+Balaji0510@users.noreply.github.com>
2025-06-23 08:51:21 +0000

    🔧 Added PySpark CI workflow

--- process_sales_data1.py
+++ process_sales_data1.py
@@ -6,7 +6,7 @@ from pyspark.sql.functions import col, sum
 spark = SparkSession.builder.appName("SalesAnalytics").getOrCreate()
 
 # ✅ Use correct relative path (from project root)
-input_path = "../Data/sales_data.csv"
+input_path = "sales_data1.csv"
 output_path = "../Output/"
 
 # ✅ Read the CSV file
@@ -19,6 +19,8 @@ df_grouped = df.groupBy("product").agg(
     sum("quantity").alias("total_quantity"),
     sum("total_amount").alias("total_sales")
 )
+
+df_grouped.show()
 
 # ✅ Write output
 df_grouped.coalesce(1).write.mode("overwrite").option("header", "true").csv(output_path)
